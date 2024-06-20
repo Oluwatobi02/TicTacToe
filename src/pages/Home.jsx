29a1b68b8c68c2ac07be7bbbae8c37071fa5f70f
Oluwatobi02 <olola73@morgan.edu>
2024-06-19 23:23:29 -0400

    made code cleaner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,8 @@ const Home = () => {
   const [username, setUsername] = useState('')
   const [roomId, setRoomId] = useState()
   const [isChecked, setIsChecked] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+
   const navigate = useNavigate()
 
   function handleCreate() {
@@ -20,7 +22,7 @@ const Home = () => {
     console.log(`${isChecked}`)
   }
   function handleJoin() {
-    if (!username || roomId) {
+    if (!username) {
       return;
     }
     navigate(`/game?name=${username}&type=guest&room=${roomId}&aimode=false`)
@@ -32,13 +34,14 @@ const Home = () => {
     <div className="home">
         <div className="home-content-container">
         <h3>Welcome to Tobi's Tic Tac Toe game</h3>
-      
-        <input type="text" min={0} name="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username: " />
-        <input type="text" name="room" value={roomId} onChange={(e) => setRoomId(e.target.value)}  placeholder="#RoomId: " pattern="\d{5}" />
+- 
+- 
+      <input type="text" min={0} name="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username: " />
+        <input type="text" name="room" value={roomId} onChange={(e) => setRoomId(e.target.value)} className="join-input"  placeholder="#00000: " pattern="\d{5}" />
         <button disabled= {isChecked} onClick={handleJoin}><ArrowForwardIcon /></button>
         <p>AI MODE:</p>
         <input type="checkbox" checked={isChecked} onChange={(e) => setIsChecked(e.target.checked)} />
-      <button className="create-button" onClick={handleCreate}>create Game</button>
+<button className="create-button" onClick={handleCreate}>create Game</button> 
         </div>
   
     </div>
